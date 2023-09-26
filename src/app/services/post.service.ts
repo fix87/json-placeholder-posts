f4f69750ec70576eb838f0bf858ae1cce74acd30
Felice Anastasio <34630622+fix87@users.noreply.github.com>
2023-09-26 20:48:14 +0200

    Removed sessionStorage and use services for storage

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 // Libraries
-import { Observable, iif, of } from 'rxjs';
+import { Observable, iif, of, tap } from 'rxjs';
 
 // Models
 import { Post } from '../models';
@@ -16,75 +16,58 @@ import { environment } from '../../environments/environment';
 })
 export class PostService {
   private baseUrl = environment.baseUrl;
+  private posts: Post[] = [];
 
   public constructor(private http: HttpClient) {}
 
   public getPosts(): Observable<Post[]> {
-    const storedPosts = this.getPostsFromSessionStorage();
     return iif(
-      () => !!storedPosts.length,
-      of(storedPosts),
-      this.http.get<Post[]>(`${this.baseUrl}/posts`)
+      () => !!this.posts.length,
+      of(this.posts),
+      this.http
+        .get<Post[]>(`${this.baseUrl}/posts`)
+        .pipe(tap((posts) => (this.posts = posts)))
     );
   }
 
   public getPost(id: number): Observable<Post | undefined> {
-    const storedPosts = this.getPostsFromSessionStorage();
     return iif(
-      () => !!storedPosts.length,
-      of(storedPosts.find((post) => post.id === id)),
+      () => !!this.posts.length,
+      of(this.posts.find((post) => post.id === id)),
       this.http.get<Post>(`${this.baseUrl}/posts/${id}`)
     );
   }
 
   public addPost(newPost: Post): Observable<Post> {
-    let storedPosts = this.getPostsFromSessionStorage();
     const greaterStoredId = this.getLastInsertedId();
-    storedPosts = [
-      ...storedPosts,
+    this.posts = [
+      ...this.posts,
       {
         ...newPost,
         id: greaterStoredId + 1,
       },
     ];
-    this.setPostsToSessionStorage(storedPosts);
     return of<Post>(newPost);
   }
 
   public editPost(body: Post): Observable<Post> {
-    const storedPosts = this.getPostsFromSessionStorage();
-    const indexToEdit = storedPosts.findIndex((post) => post.id === body.id);
-    storedPosts[indexToEdit] = body;
-    this.setPostsToSessionStorage(storedPosts);
+    const indexToEdit = this.posts.findIndex((post) => post.id === body.id);
+    const newPosts = [...this.posts];
+    newPosts[indexToEdit] = body;
+    this.posts = [...newPosts];
     return of<Post>(body);
   }
 
   public deletePost(id: number): Observable<number> {
-    const storedPosts = this.getPostsFromSessionStorage();
-    const indexToEdit = storedPosts.findIndex((post) => post.id === id);
-    storedPosts.splice(indexToEdit, 1);
-    this.setPostsToSessionStorage(storedPosts);
+    const indexToEdit = this.posts.findIndex((post) => post.id === id);
+    this.posts.splice(indexToEdit, 1);
     return of(id);
-  }
-
-  private getPostsFromSessionStorage(): Post[] {
-    let posts: Post[] = [];
-    const storedPosts = sessionStorage.getItem('posts');
-    if (storedPosts) {
-      posts = JSON.parse(storedPosts);
-    }
-    return posts;
-  }
-
-  private setPostsToSessionStorage(posts: Post[]): void {
-    sessionStorage.setItem('posts', JSON.stringify(posts));
   }
 
   private getLastInsertedId(): number {
     let greaterId = 0;
-    const storedPosts = this.getPostsFromSessionStorage();
-    if (storedPosts) {
-      const storedIds = storedPosts.map((post) => post.id);
+    if (this.posts.length) {
+      const storedIds = this.posts.map((post) => post.id);
       greaterId = Math.max(...storedIds);
     }
     return greaterId;
