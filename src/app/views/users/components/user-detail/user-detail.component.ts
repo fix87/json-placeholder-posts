dff7d85a37b45862acf4719b2c45523fe0468a78
Felice Anastasio <34630622+fix87@users.noreply.github.com>
2023-10-20 16:23:29 +0200

    Added list of posts below user detail

--- src/app/views/users/components/user-detail/user-detail.component.ts
+++ src/app/views/users/components/user-detail/user-detail.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 // Libraries
-import { Observable } from 'rxjs';
+import { Observable, forkJoin, map } from 'rxjs';
 
 // Services
 import { UserService } from 'src/app/services';
 
 // Models
-import { User } from 'src/app/models';
+import { Post, User } from 'src/app/models';
 
 @Component({
   selector: 'app-user-detail',
@@ -17,14 +17,17 @@ import { User } from 'src/app/models';
   styleUrls: ['./user-detail.component.scss'],
 })
 export class UserDetailComponent {
-  public user$: Observable<User>;
+  public data$: Observable<{ user: User; posts: Post[] }>;
 
   public constructor(
     private activatedRoute: ActivatedRoute,
     private userService: UserService
   ) {
     const userId = this.activatedRoute.snapshot.params['userId'];
-    this.user$ = this.userService.getUser(userId);
+    this.data$ = forkJoin([
+      this.userService.getUser(userId),
+      this.userService.getUserPosts(userId),
+    ]).pipe(map(([user, posts]) => ({ user, posts })));
   }
 
   public getInitials(name: string): string {
